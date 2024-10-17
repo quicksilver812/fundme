f2b9afb9334d7be9645cdf02d1917b7d185145b5
quicksilver812 <allendsouza812@gmail.com>
2024-10-17 15:13:59 +0530

    Created Aggregator interfact and getPrice() method

--- FundMe.sol
+++ FundMe.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "/Users/allendsouza/Desktop/Codes/Blockchain/fundme/AggregatorV3Interface.sol";
+
 contract FundMe{
     uint256 minimumUSD = 5e18;
 
     function fund() public payable{
         require(msg.value >= minimumUSD, "Not enough ethereum");
+    }
+
+    function getPrice()view public returns(uint256){
+
     }
 }
 
