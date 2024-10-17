1a58c6ee5d5178be5fe1b44e46a860c248a49d67
quicksilver812 <allendsouza812@gmail.com>
2024-10-17 15:17:48 +0530

    Created PriceConverter library

---
+++ PriceConverter.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "/Users/allendsouza/Desktop/Codes/Blockchain/fundme/AggregatorV3Interface.sol";
+
+library PriceConverter{
+    
+    function getPrice()view public returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price);
+    }
+
+    function convert(uint256 ethAmount) view public returns(uint256){
+        uint256 currentPrice = getPrice();
+        uint256 value = ((currentPrice*1e10)*ethAmount)/1e18;
+        return value;
+    }
+}
