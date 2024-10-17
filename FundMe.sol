77a4bbca897bcaecc03ae05e68706c649fc8f350
quicksilver812 <allendsouza812@gmail.com>
2024-10-17 15:26:18 +0530

    added onlyOwner modifier and constructor to initialise owner

--- FundMe.sol
+++ FundMe.sol
@@ -7,9 +7,13 @@ contract FundMe{
     using PriceConverter for uint256;
 
     uint256 minimumUSD = 5e18;
-
+    address public owner;
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
+
+    constructor(){
+        owner = msg.sender;
+    }
 
     function fund() public payable{
         require(msg.value.convert() >= minimumUSD, "Not enough ethereum");
@@ -17,7 +21,7 @@ contract FundMe{
         addressToAmountFunded[msg.sender]+=msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner(){
         for(uint256 fundersIndex = 0; fundersIndex<funders.length; fundersIndex++){
             address funder = funders[fundersIndex];
             addressToAmountFunded[funder] = 0;
@@ -27,6 +31,11 @@ contract FundMe{
         (bool callSuccess, ) = payable(msg.sender).call{value:address(this).balance}("");
 
         require(callSuccess, "Withdrawal Unsuccessful");
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == owner, "Not owner, cannot withdraw funds.");
+        _;
     }
 }
 
