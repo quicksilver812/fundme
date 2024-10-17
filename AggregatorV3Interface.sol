2b0b3f9d7b51c584a917ac9f6452c6909e31bf12
quicksilver812 <allendsouza812@gmail.com>
2024-10-17 15:30:36 +0530

    Created AggregatorV3interface and getPrice() method

--- AggregatorV3Interface.sol
+++ AggregatorV3Interface.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 // solhint-disable-next-line interface-starts-with-i
 interface AggregatorV3Interface {
   function decimals() external view returns (uint8);
